77db3fce0447d38953d4dbb6f96ea496719dece0
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2025-04-19 13:51:35 +0300

    Querying Basics - Select all teams

--- EntityFrameworkCore.Console/Program.cs
+++ EntityFrameworkCore.Console/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using EntityFrameworkCore.Data;
+
+// First we need an instance of the context
+var context = new FootballLeageDbContext();
+
+// Select all teams
+// Select * FROM Teams
+
+var teams = context.Teams.ToList();
+
+foreach(var team in teams)
+{
+    Console.WriteLine(team.Name);
+}
