d96245d41b3d9e853651e1c8db86f5251200b278
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2025-04-19 18:29:46 +0300

    Querying for a Single Record

--- EntityFrameworkCore.Console/Program.cs
+++ EntityFrameworkCore.Console/Program.cs
@@ -1,19 +1,43 @@
 ﻿using EntityFrameworkCore.Data;
+using Microsoft.EntityFrameworkCore;
 
 // First we need an instance of the context
-var context = new FootballLeageDbContext();
+using var context = new FootballLeageDbContext();
 
 // Select all teams
+//getAllTeams();
 
-getAllTeams();
+// Selecting a single record
+//var teamOne = await context.Coaches.FirstAsync();
+//var teamOne = await context.Coaches.FirstOrDefaultAsync();
+
+// Selecting a single record - First one un the list that meets a condition
+//var teamTwo = await context.Teams.FirstAsync(team => team.TeamId == 1);
+//var teamTwo = await context.Teams.FirstOrDefaultAsync(team => team.TeamId == 1);
+
+//var teamThree = await context.Teams.SingleAsync();
+//var teamThree = await context.Teams.SingleAsync(team => team.TeamId == 2);
+//var teamFour = await context.Teams.SingleOrDefaultAsync(team => team.TeamId == 2);
+
+// Select based on Id
+
+//var teamBasedOnId = await context.Teams.FindAsync(3);
+//if(teamBasedOnId != null)
+//{
+//    Console.WriteLine(teamBasedOnId.Name);
+//}
+
+
 
 void getAllTeams()
 {
     // Select * FROM Teams
     var teams = context.Teams.ToList();
 
-    foreach (var team in teams)
+    foreach (var t in teams)
     {
-        Console.WriteLine(team.Name);
+        Console.WriteLine(t.Name);
     }
 }
+
+
